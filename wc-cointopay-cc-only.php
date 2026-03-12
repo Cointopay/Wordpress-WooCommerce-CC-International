87f4799358ca9e0ba23b84578b07955ac55c5364
Goshila <goshila.sadaf@therightsw.com>
2026-03-12 21:16:04 +0500

    versions updated

--- wc-cointopay-cc-only.php
+++ wc-cointopay-cc-only.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Cointopay.com CC Only
  * Description: Extends WooCommerce with card payments gateway.
- * Version: 1.3.7
+ * Version: 1.3.8
  * Author: Cointopay
  * Text Domain: wc-cointopay-cc-only
  * @package  WooCommerce
